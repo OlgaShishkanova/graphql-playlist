1a8716d34cd6f053670095061e95cea4cc747094
Olga Shishkanova <Olga.Shishkanova@statravel.com>
2019-07-23 09:32:07 +0200

    lessons 27-35

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { gql } from 'apollo-boost'
 import {graphql} from 'react-apollo'
-
-const getBooksQuery = gql`
-{
-    books{
-        name
-        id
-    }
-}
-`
+import {getBooksQuery} from '../queries/queries'
+import BookDetails from './BookDetails';
 
 class BookList extends React.Component {
+    state={
+        selected: null
+    }
+    displayBooks(){
+        let data = this.props.data;
+        if(data.loading){
+            return (<div>Loading books...</div>)
+        }else{
+            return data.books.map(book => {
+                return (
+                    <li key={book.id} onClick={(e)=>{this.setState({selected: book.id})}}>{book.name}</li>
+                )
+            })
+        }
+    }
     render(){
-        console.log(this.props)
         return (
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    {this.displayBooks()}
                 </ul>
+                <BookDetails bookId={this.state.selected}/>
             </div>
         );
     }
